4d746bfc2c64684f6880e04148d511263ebbaa66
Yash Ghavghave <yashghavghave123@gmail.com>
2025-05-20 16:02:53 +0530

    ok

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 function Navbar() {
   return (
     <div>
-      <div className='h-[10vh] w-[100%] bg-black opacity-85 flex'>
+      <div className='h-[10vh] w-[100%] bg-black opacity-85 flex border-b-white'>
         <div>
-            <div id='logo' className='flex text-white'>Anjanexus</div>
+            <div id='logo' className='h-[10vh] w-[30vw] flex text-white text-3xl justify-evenly items-center'>AnjaNexus</div>
         </div>
         <div className="h-full w-[70vw] flex justify-evenly text-white items-center ">
 
